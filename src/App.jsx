5b3f2f485def3af5ce6a2e2638f40d705b0aed82
August-Trung <nguyenminhtrung01082003@gmail.com>
2025-03-02 15:17:55 +0700

    up

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+import BackgroundVideo from "./components/BackgroundVideo";
+
+function App() {
+	return (
+		<>
+			<BackgroundVideo />
+			<UI />
+			<Loader />
+			<Canvas shadows camera={{ position: [-0.5, 1, 4], fov: 45 }}>
+				<group position-y={0}>
+					<Suspense fallback={null}>
+						<Experience />
+					</Suspense>
+				</group>
+			</Canvas>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/BackgroundVideo.jsx
@@ -0,0 +1,9 @@
+const BackgroundVideo = () => {
+	return (
+		<video id="bgVideo" autoPlay loop muted playsInline>
+			<source src="/videos/loveBg.mp4" type="video/mp4" />
+		</video>
+	);
+};
+
+export default BackgroundVideo;
